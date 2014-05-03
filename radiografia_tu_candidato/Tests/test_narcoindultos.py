eb0e543dc614d1a48907dc1326f3227ecab5209d
aniversarioperu <aniversarioperu1@gmail.com>
2014-05-03 12:25:08 +0300

    test cases for matching names

--- radiografia_tu_candidato/Tests/test_narcoindultos.py
+++ radiografia_tu_candidato/Tests/test_narcoindultos.py
@@ -10,18 +10,30 @@ from radiografia_tu_candidato.narcoindultos import convert_to_minjus_url
 
 
 class NarcoindultosTest(unittest.TestCase):
+
     # extraer nombres de indultados de normas jurídicas
     def setUp(self):
         self.filename = os.path.join("Narcoindultos", "01-05-09.txt")
 
     def test_extraer_conmutados(self):
-        result = extract_conmutados(self.filename)[0]
-        obj = {
-            'nombre': 'ALZAMORA CARDOZO, VICTOR MARTIN',
-            'categoria': 'conmutado',
-            'url': 'http://spij.minjus.gob.pe/Normas/textos/010509T.pdf',
-        }
-        self.assertEqual(result, obj)
+        expected_names = [
+            u'ALZAMORA CARDOZO, VICTOR MARTIN',
+            u'NUÑEZ VILCHEZ, JOSE SAMUEL',
+            u'DEL PUERTO TARAZONA, FRANCISCO',
+            u'DE LA PUERTA TARAZONA, FRANCISCO',
+            u'DE LA PUERTA TARAZONA, FRANCISCO MIGUEL',
+            u'DE LA PUERTA TARAZONA, FRANCISCO MIGUEL ALONSO',
+            u'DE LA PUERTA TARAZONA,FRANCISCO MIGUEL ALONSO',
+        ]
+        i = 0
+        for name in expected_names:
+            result = extract_conmutados(self.filename)[i]
+            obj = dict()
+            obj['categoria'] = 'conmutado'
+            obj['url'] = 'http://spij.minjus.gob.pe/Normas/textos/010509T.pdf'
+            obj['nombre'] = name
+            i += 1
+            self.assertEqual(result, obj)
 
     def test_convert_to_minjus_url(self):
         result = convert_to_minjus_url(self.filename)
