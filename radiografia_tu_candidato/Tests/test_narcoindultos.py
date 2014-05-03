8eb4ce26526835b669af47abc48a311c90da6ec4
aniversarioperu <aniversarioperu1@gmail.com>
2014-05-03 08:40:52 +0300

    Extract metadata for individuals
    Such us: full name, category: "indultado", "conmutado", url for source of
    information.

--- radiografia_tu_candidato/Tests/test_narcoindultos.py
+++ radiografia_tu_candidato/Tests/test_narcoindultos.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append("../..")
 
 from radiografia_tu_candidato.narcoindultos import extraer_conmutados
+from radiografia_tu_candidato.narcoindultos import convert_to_minjus_url
 
 
 class NarcoindultosTest(unittest.TestCase):
@@ -15,6 +16,14 @@ class NarcoindultosTest(unittest.TestCase):
 
     def test_extraer_conmutados(self):
         result = extraer_conmutados(self.filename)[0]
+        obj = {
+                'nombre': 'ALZAMORA CARDOZO, VICTOR MARTIN',
+                'categoria': 'conmutado',
+                'url': 'http://spij.minjus.gob.pe/Normas/textos/010509T.pdf',
+                }
+        self.assertEqual(result, obj)
 
-        self.assertEqual(result, "ALZAMORA CARDOZO, VICTOR MARTIN")
+    def test_convert_to_minjus_url(self):
+        result = convert_to_minjus_url(self.filename)
+        self.assertEqual(result, 'http://spij.minjus.gob.pe/Normas/textos/010509T.pdf')
 
--- radiografia_tu_candidato/narcoindultos.py
+++ radiografia_tu_candidato/narcoindultos.py
@@ -1,13 +1,21 @@
 # coding=utf8
-import argparse
-from argparse import RawTextHelpFormatter
 import codecs
 import os.path
 import re
 
+import argparse
+from argparse import RawTextHelpFormatter
+
+
+def convert_to_minjus_url(filename):
+    filename = os.path.basename(filename)
+    filename = filename.replace("-", "")
+    filename = filename.replace(".txt", "T.pdf")
+    url = "http://spij.minjus.gob.pe/Normas/textos/" + filename
+    return url
 
 def extraer_conmutados(filename):
-    nombres = []
+    individuos = []
     pattern = "([A-Z]+\s+[A-Z]+,\s[A-Z]+\s*[A-Z]*\s*[A-Z]*)"
     if os.path.isfile(filename):
         with codecs.open(filename, "r", "utf8") as f:
@@ -15,8 +23,14 @@ def extraer_conmutados(filename):
                 if 'conmutarle' in line.lower():
                     res = re.search(pattern, line.strip())
                     if res:
-                        nombres.append(res.groups()[0])
-        return nombres
+                        nombre = res.groups()[0]
+
+                        # crear nuestro individuo
+                        obj = {'nombre': nombre}
+                        obj['categoria'] = "conmutado"
+                        obj['url'] = convert_to_minjus_url(filename)
+                        individuos.append(obj)
+        return individuos
 
 
 def main():
